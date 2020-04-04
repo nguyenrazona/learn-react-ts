788acf29963dda475ed543e1601d5eae618287cc
グエン <42921515+nguyenrazona@users.noreply.github.com>
2020-04-04 10:55:41 +0900

    update

--- src/App.tsx
+++ src/App.tsx
@@ -5,8 +5,8 @@ import './index.css'
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <h1>Hello CodeSandbox 22222</h1>
+      <h2>3333 Start editing to see some magic happen 55!</h2>
     </div>
   )
 }
